69c71c36c1710de49e3ce0d05ea761eb4c1edd33
Afifa ashfaq <afifaashfaq050@gmail.com>
2024-12-03 04:35:02 -0800

    this is the first commit

---
+++ c langauage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int X = 5 ; 
+printf("      *    \n");
+printf("    *   * \n");
+printf("   *     *  \n");
+printf("  *********\n");
+printf(" *         *\n");
+printf("*           *\n");
+	
+	
+}
